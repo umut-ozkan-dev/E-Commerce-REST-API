2c0297a8c1d85a86940b49e2ef2dcba1f1618ee7
Umut Özkan <umutozkan123456@gmail.com>
2026-09-05 18:10:25 +0300

    Implement basic customer retrieval and addition endpoints

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 
-
-
 app = FastAPI()
+
+
+@app.get("/")
+def get_customers(customers):
+    return customers
+
+
+@app.post("/")
+def add_customers(customers):
+    return customers
