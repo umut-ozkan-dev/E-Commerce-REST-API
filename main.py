2c5dfe7440345c6819fe3f1d09beb96b305819b0
Umut Özkan <umutozkan123456@gmail.com>
2026-09-06 13:27:10 +0300

    Add Django and dependencies to requirements and import Django in main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-def get_customers(customers):
-    return customers
-
-
-@app.post("/")
-def add_customers(customers):
-    return customers
+import django
