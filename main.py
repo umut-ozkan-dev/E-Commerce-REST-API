063d10f4439fd44d28533f5f0561151526f3c9d4
Umut Özkan <umutozkan123456@gmail.com>
2026-09-13 11:32:52 +0300

    Remove VSCode settings and update requirements for FastAPI integration

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+@app.get("/")
+def welcome():
+    return "hi"
+
+
+@app.get("/1")
+def welcome2():
+    return "1"
+
+
+@app.get("/2")
+def welcome3():
+    return "you entered {}"
