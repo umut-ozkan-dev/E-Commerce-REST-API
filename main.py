a109ace0c1ceed935e304d6d0a197351b0d16cbc
Umut Özkan <umutozkan123456@gmail.com>
2026-09-17 12:08:06 +0300

    Add Mako template rendering for home route and create index.html template

--- main.py
+++ main.py
@@ -4,11 +4,29 @@ from home import home_page
 from contact import contact_page
 from cart import cart_page
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
+from mako.lookup import TemplateLookup
+
+# Configure the template directory
+templates = TemplateLookup(directories=["templates"], input_encoding="utf-8")
 
 app = FastAPI()
+
+
+@app.get("/home", response_class=HTMLResponse)
+async def home2(request: Request):
+    template = templates.get_template("index.html")
+
+    html = template.render(
+        title="My FastAPI App",
+        message="Hello from FastAPI and Mako!",
+        username="e",
+        is_logged_in=True,
+    )
+
+    return HTMLResponse(content=html)
 
 
 @app.get("/", response_class=HTMLResponse)
