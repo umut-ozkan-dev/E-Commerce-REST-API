df555384babdde016cda16d687831e1a9c233b7f
Umut Özkan <umutozkan123456@gmail.com>
2026-09-18 09:20:25 +0300

    Enhance HTML structure by adding meta tags for character set and viewport across multiple pages; update product prices and improve product display in products.py

--- products.py
+++ products.py
@@ -1,12 +1,12 @@
 from typing import Dict
 
 product_list: list[Dict] = [
-    {"id": 1, "Name": "Alarm Clock", "Price": 9.90, "In Stock": True},
-    {"id": 2, "Name": "Wire Headphone", "Price": 29.90, "In Stock": True},
-    {"id": 3, "Name": "Mouse", "Price": 7.90, "In Stock": True},
-    {"id": 4, "Name": "HDW Monitor", "Price": 69.90, "In Stock": True},
-    {"id": 5, "Name": "E2 Bud Headphone", "Price": 29.90, "In Stock": True},
-    {"id": 6, "Name": "Leadwise Monitor", "Price": 79.90, "In Stock": True},
+    {"id": 1, "Name": "Alarm Clock", "Price": 9.99, "In Stock": True},
+    {"id": 2, "Name": "Wire Headphone", "Price": 29.99, "In Stock": True},
+    {"id": 3, "Name": "Mouse", "Price": 7.99, "In Stock": True},
+    {"id": 4, "Name": "HDW Monitor", "Price": 69.99, "In Stock": True},
+    {"id": 5, "Name": "E2 Bud Headphone", "Price": 29.99, "In Stock": True},
+    {"id": 6, "Name": "Leadwise Monitor", "Price": 79.99, "In Stock": True},
 ]
 
 FILEPATH = "./pages/products.html"
@@ -18,11 +18,21 @@ with open(FILEPATH) as f:
 
 for i in range(len(product_list)):
     product_page += f""" 
+                    <br>
+                    <br>
                     <div class = "items"> 
                     <div class ="inside_items"> 
-                    Name : {product_list[i]["Name"]} <br>
-                    Price : {product_list[i]["Price"]}$<br> 
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <strong>{product_list[i]["Name"]}</strong>
+                    <br>
+                    {product_list[i]["Price"]} $<br> 
                     In Stock : {product_list[i]["In Stock"]}<br> 
+                    <br>
                     <button> Add to Cart</button> 
                     </div>
                     </div>
