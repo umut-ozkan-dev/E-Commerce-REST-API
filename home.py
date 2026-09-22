f3291be209d4e9179aaa4fe5117079c893df90f6
Umut Özkan <umutozkan123456@gmail.com>
2026-09-22 11:14:54 +0300

    Refactor home page layout and styling; update product display and enhance CSS for improved aesthetics

--- home.py
+++ home.py
@@ -6,19 +6,16 @@ with open(FILEPATH) as f:
     home_page = f.read()
 
 
-for i in range(12):
+for i in range(4):
     home_page += f""" 
-                    <br>
-                    <div class = "items"> 
+        
+                    
                     <div class ="inside_items"> 
                     <img src="{product_list[i]["Img"]}" width=130>
                                         <hr>
-                    <strong><a class="item_name" href="./products/{product_list[i]["id"]}">{product_list[i]["Name"]}</a></strong>
-                    <br>
+                    <strong ><a href="./products/{product_list[i]["id"]}">{product_list[i]["Name"]}</a></strong>
+                    <br><br>
                                         <br><div class="price">
-                    ${product_list[i]["Price"]}</div> <br> 
-                    </div>
-                    </div>
-                    <br><br><br>
-
+                    ${product_list[i]["Price"]}</div> 
+                    </div>  
                     """
