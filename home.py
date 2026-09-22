b27ddfe8d33f7a266fa96df023e602a1e4b61466
Umut Özkan <umutozkan123456@gmail.com>
2026-09-22 14:12:16 +0300

    html beautified

--- home.py
+++ home.py
@@ -1,10 +1,20 @@
 FILEPATH = "./pages/home.html"
 
 from products import product_list
-from products import empty_list
+from products import empty_list, product
 
 with open(FILEPATH, "r", encoding="utf-8") as file:
     raw_html = file.read()
-    home_page = raw_html.replace("{{empty_list}}",empty_list)
-   
-    
+    empty_list = ""
+    for i in range(4):
+        product = f"""  <div class ="inside_items"> 
+                                <img src="{product_list[i]["Img"]}" width=130>
+                                <hr>
+                                <strong ><a href="./products/{product_list[i]["id"]}">{product_list[i]["Name"]}</a></strong>
+                                <br><br><br><div class="price">
+                                ${product_list[i]["Price"]}</div> 
+                                </div>  
+                                """
+        empty_list += product
+
+    home_page = raw_html.replace("{{empty_list}}", empty_list)
