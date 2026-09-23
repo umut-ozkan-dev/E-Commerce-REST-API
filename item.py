d28ad94cb821bcc063888fe1883bc780248219cb
Umut Özkan <umutozkan123456@gmail.com>
2026-09-23 08:05:39 +0300

    Add product detail page and update product list with new IDs

---
+++ item.py
@@ -0,0 +1,6 @@
+from product_list import product_list
+
+FILEPATH = "./pages/item.html"
+with open("./pages/item.html", "r", encoding="utf-8") as file:
+    raw_item_page = file.read()
+ 
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from products import product_page
+from products import product_page, product_list
 from login import login_page
 from home import home_page
 from contact import contact_page
 from cart import cart_page
-
+from item import raw_item_page
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
@@ -13,6 +13,7 @@ from mako.lookup import TemplateLookup
 templates = TemplateLookup(directories=["templates"], input_encoding="utf-8")
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -38,6 +39,19 @@ def login():
 @app.get("/contact", response_class=HTMLResponse)
 def contact():
     return contact_page
+
+
+@app.get("/products/{id}", response_class=HTMLResponse)
+def get_products(id: int):
+    with open("./pages/item.html", "r", encoding="utf-8") as file:
+        raw_item_page = file.read()
+
+    raw_item_page = raw_item_page.replace("{{id}}", str(product_list[id]["id"]))
+    raw_item_page = raw_item_page.replace("{{Name}}", str(product_list[id]["Name"]))
+    raw_item_page = raw_item_page.replace("{{Price}}", str(product_list[id]["Price"]))
+    raw_item_page = raw_item_page.replace("{{Img}}", str(product_list[id]["Img"]))
+    item_page = raw_item_page
+    return item_page
 
 
 @app.get("/newstyles.css")
