60f77dd8d7388b808e6bb715da4786ecc707c132
Umut Özkan <umutozkan123456@gmail.com>
2026-09-23 20:20:46 +0300

    Update home page layout and styling, add discount message, and enhance image display

--- home.py
+++ home.py
@@ -6,8 +6,9 @@ from products import empty_list, product
 with open(FILEPATH, "r", encoding="utf-8") as file:
     raw_html = file.read()
     empty_list = ""
-    
-    for i in range(10):
+
+    home_page_items_list = [19, 15, 0, 32, 27, 40, 18, 22, 9, 23]
+    for i in home_page_items_list:
         product = f"""  <div class ="inside_items"> 
                                 <img src="{product_list[i]["Img"]}" width=130>
                                 <hr>
