0d98ff58d9556f7023f456fd320540c77f32cfa3
Umut Özkan <umutozkan123456@gmail.com>
2026-09-26 07:27:38 +0300

    Huge refactoring by removing all the html from pyhon files

--- home.py
+++ home.py
@@ -9,14 +9,12 @@ with open(FILEPATH, "r", encoding="utf-8") as file:
 
     home_page_items_list = [19, 15, 7, 21, 27, 40, 18, 22, 25, 23]
     for i in home_page_items_list:
-        product = f"""  <div class ="inside_items"> 
-                                <img src="{product_list[i]["Img"]}" width=130>
-                                <hr>
-                                <strong ><a href="./products/{product_list[i]["id"]}">{product_list[i]["Name"]}</a></strong>
-                                <br><br><br><div class="price">
-                                ${product_list[i]["Price"]}</div> 
-                                </div>  
-                                """
-        empty_list += product
+        with open("./templates/product.html", "r", encoding="utf-8") as product_html:
+            product = product_html.read()
+            product = product.replace("{{Image}}", product_list[i]["Img"])
+            product = product.replace("{{Id}}", str(product_list[i]["id"]))
+            product = product.replace("{{Name}}", product_list[i]["Name"])
+            product = product.replace("{{Price}}", str(product_list[i]["Price"]))
+            empty_list += product
 
     home_page = raw_html.replace("{{empty_list}}", empty_list)
--- products.py
+++ products.py
@@ -8,14 +8,12 @@ with open(FILEPATH, "r", encoding="utf-8") as file:
     raw_html = f"{file.read()}"
     empty_list = ""
     for i in range(len(product_list)):
-        product = f"""  <div class ="inside_items"> 
-                        <img src="{product_list[i]["Img"]}" width=130>
-                        <hr>
-                        <strong ><a href="./products/{product_list[i]["id"]}">{product_list[i]["Name"]}</a></strong>
-                        <br><br><br><div class="price">
-                        ${product_list[i]["Price"]}</div> 
-                        </div>  
-                        """
-        empty_list += product
-        
+        with open("./templates/product.html", "r", encoding="utf-8") as product_html:
+            product = product_html.read()
+            product = product.replace("{{Image}}", product_list[i]["Img"])
+            product = product.replace("{{Id}}", str(product_list[i]["id"]))
+            product = product.replace("{{Name}}", product_list[i]["Name"])
+            product = product.replace("{{Price}}", str(product_list[i]["Price"]))
+            empty_list += product
+
     product_page = raw_html.replace("{{empty_list}}", empty_list)
