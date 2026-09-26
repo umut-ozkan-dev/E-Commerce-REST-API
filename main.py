de43d4bd3d4f4f78c2f58f08e6ba6730ac1b50fd
Umut Özkan <umutozkan123456@gmail.com>
2026-09-26 06:59:48 +0300

    add 404 exception message

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from home import home_page
 from contact import contact_page
 from cart import cart_page
 from item import raw_item_page
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 
@@ -39,21 +39,19 @@ def contact():
 
 @app.get("/products/{id}", response_class=HTMLResponse)
 def get_products(id: int):
-    with open("./pages/item.html", "r", encoding="utf-8") as file:
-        raw_item_page = file.read()
-
-    raw_item_page = raw_item_page.replace("{{id}}", str(product_list[id]["id"]))
-    raw_item_page = raw_item_page.replace("{{Name}}", str(product_list[id]["Name"]))
-    raw_item_page = raw_item_page.replace("{{Price}}", str(product_list[id]["Price"]))
-    raw_item_page = raw_item_page.replace("{{Img}}", str(product_list[id]["Img"]))
-    raw_item_page = raw_item_page.replace(
-        "{{Category}}", str(product_list[id]["Category"])
-    )
-    raw_item_page = raw_item_page.replace(
-        "{{Quantity}}", str(product_list[id]["Quantity"])
-    )
-    item_page = raw_item_page
-    return item_page
+    for product in product_list:
+        if product.get("id") == id:
+            with open("./pages/item.html", "r", encoding="utf-8") as file:
+                raw = file.read()
+                raw = raw.replace("{{id}}", str(product_list[id]["id"]))
+                raw = raw.replace("{{Name}}", str(product_list[id]["Name"]))
+                raw = raw.replace("{{Price}}", str(product_list[id]["Price"]))
+                raw = raw.replace("{{Img}}", str(product_list[id]["Img"]))
+                raw = raw.replace("{{Category}}", str(product_list[id]["Category"]))
+                raw = raw.replace("{{Quantity}}", str(product_list[id]["Quantity"]))
+                item_page = raw
+            return item_page
+    raise HTTPException(status_code=404)
 
 
 @app.get("/styles.css")
